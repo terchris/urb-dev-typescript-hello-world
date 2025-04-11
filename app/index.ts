0b320e9cd1e2f817390c873412b31db3b35acdca
terje.christensen <terje.christensen@users.noreply.github.com>
2025-04-11 10:18:53 +0000

    changed versions in the build

--- app/index.ts
+++ app/index.ts
@@ -1,3 +1,10 @@
+/*
+file: app/index.ts
+This is a basic web server using Express and TypeScript
+It serves a simple "Hello world" message on the root URL
+It is a simple example to demonstrate how to set up a TypeScript project with Express
+and how to run a basic web server
+*/
 import express from 'express';
 
 const app = express();
