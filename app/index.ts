d29e2743238d37af36dade32c47bb55437557f99
terje.christensen <terje.christensen@users.noreply.github.com>
2025-04-16 15:48:09 +0000

    local dev works and push to kuvernetes works

--- app/index.ts
+++ app/index.ts
@@ -23,7 +23,7 @@ const timeDateString = new Date().toLocaleString('en-GB', {
 }).replace(',', '').replace(/(\d{2}:.*) (\d{2}\/.*)/,'Time: $1 Date: $2');
 
 app.get('/', (_, res) => {
-  res.send('Hello world ! Template: typescript-basic-webserver. ' +
+  res.send('Hello world I develop locally and push to kubernetes ! Template: typescript-basic-webserver. ' +
     'Time is: ' + timeDateString) 
 });
 
